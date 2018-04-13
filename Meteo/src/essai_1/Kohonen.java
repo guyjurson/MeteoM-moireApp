0d649f9d4d95a5dc3e57e84e8948abd8f909d664
Charles Ris <Charles@MacBook-Pro-de-Charles-2.local>
2018-04-13 12:53:12 +0200

    ajout normalisation données

---
+++ Meteo/src/essai_1/Kohonen.java
@@ -0,0 +1,14 @@
+package essai_1;
+
+/**
+ * Created by Charles on 13/04/2018.
+ */
+public class Kohonen {
+
+
+    public double distanceEuclidienne(){
+        return Math.sqrt(Math.pow(2,...) +  Math.pow(2,...)+ Math.pow(2,...)+Math.pow(2,...));
+    }
+
+
+}
